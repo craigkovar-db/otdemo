b58323a55b45c91f357450453d41a0a355c49fbe
Craig Kovar <craig.kovar@databricks.com>
2024-08-26 17:07:18 -0500

    Updated to demo volume

--- helloworld_volume.py
+++ helloworld_volume.py
@@ -1,1 +1,9 @@
-print("Hello World from Volumes")
+import argparse
+parser=argparse.ArgumentParser(description="sample argument parser")
+parser.add_argument('-f', '--file')
+args=parser.parse_args()
+
+filename = args.file
+with open(filename) as f:
+    content = f.readline().strip()
+    print(f"Hello World from Volumes to {content}")
